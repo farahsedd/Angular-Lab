450b8efe8d601aaff4490ddaaaf5d80b80143eb0
farah seddik <farahseddik500@gmail.com>
2023-11-11 16:29:18 +0100

    first commit

---
+++ tp1/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MiniWordComponent } from './mini-word/mini-word.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MiniWordComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
