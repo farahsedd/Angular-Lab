065e518f9fb60ea57a1eda33fc08e727f7210235
nadamankai <nada.mankai@insat.ucar.tn>
2023-11-12 17:57:05 +0100

    adding card component content

--- tp1/src/app/cv-management/card/card.component.ts
+++ tp1/src/app/cv-management/card/card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+  user = {
+    name: 'John',
+    firstname: 'Doe',
+    age: 25,
+    description:'bfdqhsdbf fqdvqsk dfvkjqdhfnv dfsvkjqdnkv dfsvqdkf'
+    // Add other properties as needed
+  };
+  ngOnInit(): void {
+  }
 
 }
