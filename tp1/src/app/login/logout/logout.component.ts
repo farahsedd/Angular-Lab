16e20821931901db180f092e4c61c5a7fc3d597b
farah seddik <farahseddik500@gmail.com>
2023-11-13 00:06:56 +0100

    add login to navbar

--- tp1/src/app/login/logout/logout.component.ts
+++ tp1/src/app/login/logout/logout.component.ts
@@ -4,21 +4,25 @@ import { AuthentificationService } from '../Services/authentification.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
-import { appStore } from '../store/user.store';
+import { AppState, appStore } from '../store/user.store';
 import { Observable } from 'rxjs/internal/Observable';
 import { authActions } from '../store/user.actions';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
+
 export class LogoutComponent {
   isAuthenticated$: Observable<boolean>;
 
+  
   constructor(private http: HttpClient,private router: Router,private authService: AuthentificationService,private store: Store) {
-
-  this.isAuthenticated$ = this.store.pipe(select((state)=>!!appStore.user));
+// hethi 8alta !!!!!!!!!!!!!!!!!!!!!
+  this.isAuthenticated$ = this.store.pipe(map((state)=>!!appStore.user));
+  console.log(appStore.user+"   ")
   console.log(this.isAuthenticated$)
 
   }
