f89585d471704002b7599f5d3686e67ca60d33ec
Maissene Zairi <maissen.zairi@gmail.com>
2023-11-26 01:05:53 +0100

    Added age tabs in cv list.

--- tp1/src/app/cv-management/Cv.ts
+++ tp1/src/app/cv-management/Cv.ts
@@ -5,7 +5,7 @@ export type Cv = {
   cin: string;
   job: string;
   path: string;
-  age: string;
+  age: number;
   description: string;
   followers: string;
   following: string;
--- tp1/src/app/cv-management/cv-tech/cv-tech.component.ts
+++ tp1/src/app/cv-management/cv-tech/cv-tech.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
 import { Observable } from 'rxjs';
 import {CvService} from "../cv.service";
 import {EmbaucheService} from "../embauche.service";
@@ -16,10 +17,20 @@ export class CvTechComponent {
   embauchees : Cv[] = [];
   cvs$: Observable<Cv[]> = new Observable<Cv[]>();
   selectedCv: Cv|null = null;
+  cvsAbove40$: Observable<Cv[]> = new Observable<Cv[]>();
+  cvsBelow40$: Observable<Cv[]> = new Observable<Cv[]>();
+  shouldDisplayAbove40: boolean = false;
 
   async ngOnInit(): Promise<void> {
     this.cvs$ = this.cvService.getAllCvs();
     this.embauchees = this.embaucheService.getEmbauchees();
+    this.cvsAbove40$ = this.cvs$. pipe(
+      map((cvs) => cvs.filter((cv) => cv.age && cv.age > 40))
+    );
+
+    this.cvsBelow40$ = this.cvs$.pipe(
+      map((cvs) => cvs.filter((cv) => cv.age && cv.age <= 40))
+    );
   }
 
   onSelect(event:Cv): void {
@@ -27,6 +38,9 @@ export class CvTechComponent {
   }
   onAddEmbauche(event:Cv[]): void {
     this.embauchees = event;
+  }
+  changeTab(event: boolean): void {
+    this.shouldDisplayAbove40 = event;
   }
 
 }
